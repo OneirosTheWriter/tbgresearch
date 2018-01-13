717d0db864f61264a1cacee8e0a8204263bcdb9d
Oneiros <cryshalo@gmail.com>
2018-01-13 17:28:39 +0800

    Updated TechPriority to fill out members.

--- TBGResearch/Classes/TechPriority.cs
+++ TBGResearch/Classes/TechPriority.cs
@@ -3,10 +3,52 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 
 namespace TBGResearch.Classes
 {
-    class TechPriority
+    public class TechPriority
     {
+        /// <summary>
+        /// The Current stat of research progress.
+        /// </summary>
+        [JsonIgnore]
+        public EntityStateOfArt Status { get; set; }
+
+        /// <summary>
+        /// Additional Priority assigned to chasing landmark techs
+        /// </summary>
+        public double LandmarkTechPriority { get; set; }
+
+        private Dictionary<PartType, double> _PartTechPriority = new Dictionary<PartType, double>();
+        /// <summary>
+        /// A list of special part priorities. If not found in here, priority is 1.
+        /// </summary>
+        public Dictionary<PartType, double> PartTechPriority
+        {
+            get
+            {
+                return _PartTechPriority;
+            }
+            set
+            {
+                _PartTechPriority = value;
+            }
+        }
+
+        // === Methods ===
+
+        public double GetPartPriority(PartType type)
+        {
+            if (PartTechPriority.ContainsKey(type))
+                return PartTechPriority[type];
+            else
+                return 1;
+        }
+
+        public double AssignPriority(ResearchFrame target)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
